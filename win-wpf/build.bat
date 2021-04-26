e87fdf6c902d47b1d4060a57c62742dfedb159f5
zhwin <zh@win.com>
2021-04-26 14:23:08 +0800

    my msbuild position

--- win-wpf/build.bat
+++ win-wpf/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 WHERE msbuild.exe > nul 2>&1
-IF %ERRORLEVEL% NEQ 0 set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
+IF %ERRORLEVEL% NEQ 0 set PATH=%PATH%;D:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
 
 WHERE msbuild.exe > nul
 IF %ERRORLEVEL% NEQ 0 echo msbuild.exe not found; exit /b 1
